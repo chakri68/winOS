80d4b27661842f0d617700360658cb2306eb0ba3
chakri68 <chakridevireddy69@gmail.com>
2023-07-26 19:40:19 +0530

    [🔥 feat]: Added initalPosition for DesktopIcons

--- src/components/DesktopIcon/DesktopIcon.tsx
+++ src/components/DesktopIcon/DesktopIcon.tsx
@@ -9,10 +9,20 @@ export type DesktopIconProps = {
   icon?: React.ReactNode;
   onClick?: () => void;
   parentRef: React.RefObject<HTMLDivElement>;
+  className?: string;
+  initialPosition?: { x: number; y: number };
 };
 
-function DesktopIcon({ icon, label, onClick, parentRef }: DesktopIconProps) {
-  const { ref, style } = useDraggable(parentRef);
+function DesktopIcon({
+  icon,
+  label,
+  onClick,
+  parentRef,
+  className,
+  initialPosition,
+  ...rest
+}: DesktopIconProps & React.HTMLProps<HTMLDivElement>) {
+  const { ref, style } = useDraggable(parentRef, initialPosition);
   const [isFocused, setIsFocused] = useState<boolean>(false);
 
   if (parentRef === undefined) {
@@ -23,12 +33,17 @@ function DesktopIcon({ icon, label, onClick, parentRef }: DesktopIconProps) {
 
   return (
     <div
-      className={clsx(styles.DesktopIcon, isFocused && styles.selected)}
+      className={clsx(
+        className,
+        styles.DesktopIcon,
+        isFocused && styles.selected
+      )}
       ref={ref}
       style={{ ...style }}
       tabIndex={1}
       onFocus={() => setIsFocused(true)}
       onBlur={() => setIsFocused(false)}
+      {...rest}
     >
       <div className={styles.DesktopIcon__icon}>{icon ?? <IconFile />}</div>
       <div className={styles.DesktopIcon__name}>{label}</div>
